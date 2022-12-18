dd6bea1ce50c8c4d6c83cc9de064311cb94f6c20
PaulDom <54893606+PaulDom@users.noreply.github.com>
2022-12-18 11:20:44 +0300

    Add files via upload

---
+++ Platform.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public bool isActive = false;
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isActive = true;
+        }
+
+        if (other.tag == "PlatformStop")
+        {
+            direction *= -1;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isActive = false;
+        }
+    }
+
+}
